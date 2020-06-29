090e5112fc96aec55069a9e65bd5b7e6512be885
matheusrmeloo <matheusrodrigues030717@gmail.com>
2020-06-29 20:21:26 -0300

    Adding Test

--- test/tests/videogame/videogame.js
+++ test/tests/videogame/videogame.js
@@ -1,5 +1,5 @@
 import {
-    getVideogames, registerAdmin, loginAdmin
+    getVideogames, registerAdmin, loginAdmin, getVideogamesLayout
 } from '../../methods';
 
 import chai from 'chai';
@@ -8,7 +8,7 @@ import { detectValidationErrors, mochaAsync } from '../../utils';
 const expect = chai.expect;
 
 context('Videogame', async () => {
-    var admin
+    var admin, user, app
 
     before( async () =>  {
         var postDataAdmin = {
@@ -19,12 +19,23 @@ context('Videogame', async () => {
         }
         admin = await registerAdmin(postDataAdmin);
         admin = (await loginAdmin(postDataAdmin)).data.message;
+        user = {id: "5e5bfd189517230021a8c99a", bearerToken: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkF1dGgvNWU1YmZkMTg5NTE3MjMwMDIxYThjOTlhIiwidGltZSI6MTU5NjA2MzMyODY2NSwiaWF0IjoxNTkzNDcxMzI4fQ.bjiczYiMZK4DW51Uvo3BIsjKLGvLwXv9BHqNzvt73mx5c_mIAmhVxzZCeWtG157i8fJGYIy9YkVuub7TrwiZXA"},
+        app = {id: "5e48d6a928c1af0021c366d6"}
     });
 
     it('should get All Videogames', mochaAsync(async () => {
         var res = await getVideogames({
             admin : admin.id
         }, admin.bearerToken , {id : admin.id});
+        detectValidationErrors(res);
+        expect(res.data.status).to.equal(200);
+    }));
+
+    it('should get All Videogames Layout', mochaAsync(async () => {
+        var res = await getVideogamesLayout({
+            user : user.id,
+            app : app.id,
+        }, user.bearerToken , {id : user.id});
         detectValidationErrors(res);
         expect(res.data.status).to.equal(200);
     }));
