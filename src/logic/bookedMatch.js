e18861e7f53bb4ba2b3d46c97ffc785abaf5ef12
Paulo <paulohr.abreu@gmail.com>
2020-11-10 17:43:12 -0300

    .data

--- src/logic/bookedMatch.js
+++ src/logic/bookedMatch.js
@@ -96,7 +96,7 @@ const processActions = {
                     matchesId.push(matchResult.match.external_id)
                 }
                 let pandaScore  = await axios.get(`https://api.pandascore.co/betting/matches?filter%5Bid%5D=${matchesId.toString()}&per_page=${params.size}&sort=${(params.sort == "DESC" ? "-scheduled_at" : "scheduled_at")}&token=${PANDA_SCORE_TOKEN}`);
-                let marketMatch = await axios.get(`https://tangerine.pandascore.co/api/matches/winner_markets?match_ids=${matchesId.toString()}&token=${PANDA_SCORE_TOKEN}`);
+                let marketMatch = (await axios.get(`https://tangerine.pandascore.co/api/matches/winner_markets?match_ids=${matchesId.toString()}&token=${PANDA_SCORE_TOKEN}`)).data;
                 console.log(marketMatch);
                 pandaScore.data = pandaScore.data.map((match) => {
                     let oddsResult = matches.find(resultMatch => resultMatch.match.external_id == match.id);
